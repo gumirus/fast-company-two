22545ea5a2ef59aae9e5b88f3d4b07c19509fecc
Ruslan Gumirov <gumirus29@gmail.com>
2022-11-10 17:24:12 +0100

    5_урок-Фильтрация

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,78 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
+import User from "./user";
 import api from "../api";
+import GroupList from "./groupList";
+import SearchStatus from "./searchStatus";
+const Users = ({ users: allUsers, ...rest }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [professions, setProfession] = useState();
+  const [selectedProf, setSelectedProf] = useState();
 
-const Users = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
+  const pageSize = 2;
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfession(data));
+  }, []);
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedProf]);
 
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId));
+  const handleProfessionSelect = (item) => {
+    setSelectedProf(item);
   };
 
-  const renderPhrase = (number) => {
-    const lastOne = Number(number.toString().slice(-1));
-    if (number > 4 && number < 15) return "человек тусанет";
-    if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
-    if (lastOne === 1) return "человек тусанет";
-    return "человек тусанет";
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+  const filteredUsers = selectedProf
+    ? allUsers.filter((user) => user.profession._id === selectedProf._id)
+    : allUsers;
+
+  const count = filteredUsers.length;
+  const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+  const clearFilter = () => {
+    setSelectedProf();
   };
 
   return (
-    <>
-      <h2>
-        <span
-          className={
-            "badge " + (users.length > 0 ? "bg-primary" : "bg-danger")
-          }>
-          {users.length > 0
-            ? `${
-                users.length + " " + renderPhrase(users.length)
-              } с тобой сегодня`
-            : "Никто с тобой не тусанет"}
-        </span>
-      </h2>
-
-      {users.length > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map((item) => (
-                    <span
-                      className={"badge m-1 bg-" + item.color}
-                      key={item._id}>
-                      {item.name}
-                    </span>
-                  ))}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate} /5</td>
-                <td>
-                  <button
-                    onClick={() => handleDelete(user._id)}
-                    className="btn btn-danger">
-                    delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className="d-flex">
+      {professions && (
+        <div className="d-flex flex-column flex-shrink-0 p-3">
+          <GroupList
+            selectedItem={selectedProf}
+            items={professions}
+            onItemSelect={handleProfessionSelect}
+          />
+          <button className="btn btn-secondary mt-2" onClick={clearFilter}>
+            {" "}
+            Очистить
+          </button>
+        </div>
       )}
-    </>
+      <div className="d-flex flex-column">
+        <SearchStatus length={count} />
+        {count > 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Избранное</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {usersCrop.map((user) => (
+                <User {...rest} {...user} key={user._id} />
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div className="d-flex justify-content-center">
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </div>
   );
+};
+Users.propTypes = {
+  users: PropTypes.array,
 };
 
 export default Users;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
-import Users from "./app/components/users";
 import "./index.css";
-
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Users />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
