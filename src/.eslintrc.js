f3f098a9b7b437ebc3588b9bd549606261ef5266
Ruslan Gumirov <gumirus29@gmail.com>
2022-11-10 17:40:25 +0100

    5_урок-Фильтрация

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-    },
-    plugins: ["react"],
-    rules: {
-        indent: ["error", 2, { SwitchCase: 1 }],
-        semi: [2, "always"],
-        "space-before-function-paren": [
-            "error",
-            { anonymous: "always", named: "never" }
-        ],
-        quotes: ["error", "double", { allowTemplateLiterals: true }]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    semi: [2, "always"],
+    "space-before-function-paren": [
+      "error",
+      { anonymous: "always", named: "never" },
+    ],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+  },
 };
